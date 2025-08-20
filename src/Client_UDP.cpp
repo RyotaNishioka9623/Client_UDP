a727db47a7dc70c239fb9426c684b681feaa7c1a
RyotaNishioka <ryota.nishioka9623@gmail.com>
2025-08-20 13:44:16 +0900

    0820/02

--- src/Client_UDP.cpp
+++ src/Client_UDP.cpp
@@ -9,10 +9,14 @@ int main ()
    int port = 1910;
    const char* rasppi01 = "192.168.50.12"; 
    const char* rasppi02 = "192.168.50.56";
+   const char* rasppi03 = "192.168.50.31"; 
+   const char* rasppi04 = "192.168.50.32";
 
    int sockfd;
    sockaddr_in addr01;
    sockaddr_in addr02;
+   sockaddr_in addr03;
+   sockaddr_in addr04;
 
    sockfd = socket(AF_INET, SOCK_DGRAM, 0); // ★SOCK_DGRAMに修正
    if (sockfd < 0) 
@@ -32,12 +36,28 @@ int main ()
    addr02.sin_family = AF_INET;
    addr02.sin_port = htons(port);
    addr02.sin_addr.s_addr = inet_addr(rasppi02);
+
+   // rasppi03のアドレスを設定
+   memset(&addr03, 0, sizeof(addr03));
+   addr03.sin_family = AF_INET;
+   addr03.sin_port = htons(port);
+   addr03.sin_addr.s_addr = inet_addr(rasppi03);
+   
+   // rasppi04のアドレスを設定
+   memset(&addr04, 0, sizeof(addr04));
+   addr04.sin_family = AF_INET;
+   addr04.sin_port = htons(port);
+   addr04.sin_addr.s_addr = inet_addr(rasppi04);
    
    char msg_send01[128] = "Hello, World!";
    char msg_send02[128] = "Hello, Ikuto!";
+   char msg_send03[128] = "Hello, Udon!";
+   char msg_send04[128] = "Hello, Tarutaru!";
 
    sendto(sockfd, msg_send01, std::size(msg_send01), 0,(sockaddr*)&addr01,sizeof(addr01)); 
    sendto(sockfd, msg_send02, std::size(msg_send02), 0,(sockaddr*)&addr02,sizeof(addr02));
+   sendto(sockfd, msg_send03, std::size(msg_send03), 0,(sockaddr*)&addr03,sizeof(addr03));
+   sendto(sockfd, msg_send04, std::size(msg_send04), 0,(sockaddr*)&addr04,sizeof(addr04));
 
    close(sockfd); 
    return 0;
