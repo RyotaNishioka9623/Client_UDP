d55262847c9d8d684fae9e0592d458f448d7829a
RyotaNishioka <ryota.nishioka9623@gmail.com>
2025-08-20 13:29:48 +0900

    0820/01

--- src/Client_UDP.cpp
+++ src/Client_UDP.cpp
@@ -7,10 +7,12 @@
 int main ()
 {
    int port = 1910;
-   const char* ipv4 = "192.168.50.1"; 
+   const char* rasppi01 = "192.168.50.12"; 
+   const char* rasppi02 = "192.168.50.56";
 
    int sockfd;
-   sockaddr_in addr;
+   sockaddr_in addr01;
+   sockaddr_in addr02;
 
    sockfd = socket(AF_INET, SOCK_DGRAM, 0); // ★SOCK_DGRAMに修正
    if (sockfd < 0) 
@@ -19,15 +21,24 @@ int main ()
       return -1;
    }
 
-   memset(&addr, 0, sizeof(addr));
-   addr.sin_family = AF_INET;
-   addr.sin_port = htons(port);
-   addr.sin_addr.s_addr = inet_addr(ipv4);
+   // rasppi01のアドレスを設定
+   memset(&addr01, 0, sizeof(addr01));
+   addr01.sin_family = AF_INET;
+   addr01.sin_port = htons(port);
+   addr01.sin_addr.s_addr = inet_addr(rasppi01);
+   
+   // rasppi02のアドレスを設定
+   memset(&addr02, 0, sizeof(addr02));
+   addr02.sin_family = AF_INET;
+   addr02.sin_port = htons(port);
+   addr02.sin_addr.s_addr = inet_addr(rasppi02);
+   
+   char msg_send01[128] = "Hello, World!";
+   char msg_send02[128] = "Hello, Ikuto!";
 
-   char msg_send[128] = "Hello, server!";
+   sendto(sockfd, msg_send01, std::size(msg_send01), 0,(sockaddr*)&addr01,sizeof(addr01)); 
+   sendto(sockfd, msg_send02, std::size(msg_send02), 0,(sockaddr*)&addr02,sizeof(addr02));
 
-   sendto(sockfd, msg_send, std::size(msg_send), 0,(sockaddr*)&addr,sizeof(addr)); // ★sockfdに修正
-
-   close(sockfd); // ★sockfdに修正
+   close(sockfd); 
    return 0;
 }
